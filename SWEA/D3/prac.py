f4abb5c817f277911865f486a0b6670115d07084
meganatc7 <meganatc7@gmail.com>
2021-08-05 22:38:50 +0900

    Add swea

--- SWEA/D3/prac.py
+++ SWEA/D3/prac.py
@@ -1,16 +1,1 @@
-import sys
-sys.stdin = open('data/input11.txt','r')
-
-TC = int(input())
-for t in range(1, TC+1):
-    N = int(input())
-    arr = list(map(int,input().split()))
-
-    dp = [1 for i in range(N)]
-
-    for i in range(N):
-        for j in range(i):
-            if arr[i] > arr[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
-
-    print('#{} {}'.format(t,max(dp)))
+#prac
