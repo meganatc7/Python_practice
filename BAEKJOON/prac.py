ff721898076ff12a488e510371de0297c5c2b5e0
meganatc7 <meganatc7@gmail.com>
2021-08-22 15:55:53 +0900

    Add baekjoon

--- BAEKJOON/prac.py
+++ BAEKJOON/prac.py
@@ -1,4 +1,24 @@
-print('prac')
-print('prac')
-print('prac')
-print('prac')
+def solution(v):
+    answer = []
+    r = {}
+    c = {}
+    for spot in v:
+        if r.get(str(spot[0])):
+            r[str(spot[0])] += 1
+        else:
+            r[str(spot[0])] = 1
+
+        if c.get(str(spot[1])):
+            c[str(spot[1])] += 1
+        else:
+            c[str(spot[1])] = 1
+
+    for key, value in r.items():
+        if value == 1:
+            answer.append(int(key))
+
+    for key, value in c.items():
+        if value == 1:
+            answer.append(int(key))
+
+    return answer
